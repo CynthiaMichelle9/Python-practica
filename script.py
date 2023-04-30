23c948042a05e6c5085303cadde622273b8f2552
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2023-04-30 00:05:46 -0400

    Descripción del proyecto

---
+++ script.py
@@ -0,0 +1,15 @@
+# Elaborar un programa que recorra una lista con los nombres de 10 de sus futuros usuarios de tu aplicación (pueden ser personas, pacientes, organizaciones sociales o instituciones públicas).
+
+# Mediante una función, a todos los usuarios se les creará una cuenta automáticamente.
+
+# Asigne una contraseña para cada cuenta. La contraseña debe ser creada con random y debe cumplir con los siguientes criterios: mayúsculas, minúsculas y números.
+
+# Cada cuenta debe guardarse en una nueva variable con su respectiva contraseña.
+
+# Por cada cuenta debe pedir un número telefónico para contactarse.
+
+# El programa no terminará de preguntar por los números hasta que todas las organizaciones tengan un número de contacto asignado.
+
+#El programa debe verificar que el número telefónico tenga 8 dígitos numéricos.
+
+#Se debe guardar como un string
